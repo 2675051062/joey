c095be5e9157aaf9bf7371705081a1c9c7b2c84f
2675051062 <48314784+2675051062@users.noreply.github.com>
2026-02-02 23:04:54 +0800

    Added package-lock.json

---
+++ joey-角色创作室/tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+  content: [
+    "./index.html",
+    "./index.tsx",
+    "./App.tsx",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        fredoka: ['Fredoka', 'sans-serif'],
+      },
+      colors: {
+        warm: '#FDF6F0',
+      },
+    },
+  },
+  plugins: [],
+}
